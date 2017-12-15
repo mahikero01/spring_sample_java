4c5a923b6e8710cd4613121c88309e27bcba5be7
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-15 13:42:56 -0500

    Prototype Java Config

--- src/main/java/org/mahikero01/service/CustomerServiceImpl.java
+++ src/main/java/org/mahikero01/service/CustomerServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service("customerService")
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CustomerServiceImpl implements CustomerService {
 
 	
