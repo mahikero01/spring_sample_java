fb938d417eccca28c7b0625edaa8309b9464bd45
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-15 11:05:35 -0500

    Autowired Setup

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,16 +3,19 @@ import org.mahikero01.repository.HibernateCustomerRepositoryImpl;
 import org.mahikero01.service.CustomerService;
 import org.mahikero01.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 @Configuration
+@ComponentScan({"org.mahikero01"})
 public class AppConfig {
 	
 	@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
-		CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
-		//service.setCustomerRepository(getCustomerRepository());
+		//CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+		CustomerServiceImpl service = new CustomerServiceImpl();
+		service.setCustomerRepository(getCustomerRepository());
 		return service;
 	}
 	    
