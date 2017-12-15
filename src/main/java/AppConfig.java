b1aece9ca95f5553ad4f1e1b98407e92946f6d73
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-15 09:25:49 -0500

    @Configuration

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,4 +1,14 @@
+import org.mahikero01.service.CustomerService;
+import org.mahikero01.service.CustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-
+	
+	@Bean(name = "customerService")
+	public CustomerService getCustomerService() {
+		return new CustomerServiceImpl();
+	}
+	                         
 }
