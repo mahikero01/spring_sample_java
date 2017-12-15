41c1ef78eb56293e40cb95ae74402cc2f7c892ce
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-15 14:13:06 -0500

    Properties - Java Cinfig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -5,11 +5,19 @@ import org.mahikero01.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Service;
 
 @Configuration
 @ComponentScan({"org.mahikero01"})
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer(); 
+	}
 	
 //	@Bean(name = "customerService")
 	//public CustomerService getCustomerService() {
--- src/main/java/org/mahikero01/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/org/mahikero01/repository/HibernateCustomerRepositoryImpl.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.mahikero01.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	@Value("${dbUsername}")
+	private String dbusername;
 	
 	/* (non-Javadoc)
 	 * @see org.mahikero01.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll() {
+		
+		System.out.println(dbusername);
 		
 		List<Customer> customers = new ArrayList<>();
 		Customer customer = new Customer();
